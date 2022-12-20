74668b2b961e6ad94d20150d3e79572ee2834c06
StefanoBlancoA <114984307+StefanoBlancoA@users.noreply.github.com>
2022-12-20 11:54:00 +0100

    Update Productor.java

--- Productor.java
+++ Productor.java
@@ -10,19 +10,15 @@ public class Productor extends Thread {
 	{
 		this.dep = dep;
 	}
-	public synchronized void run()
+	public void run()
 	{
 		for(int i=1; i<=15; i++) {
 			double litros = Math.random()*1000;
 			dep.prodLlenaDep(litros);
 			System.out.println("Productor llena el depósito "+litros+" litros.");
-			notify();
 			try {
-				wait();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+				sleep(500);
+			}catch(Exception e) {}
 		}
 	}
 }
